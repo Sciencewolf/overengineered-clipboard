dc343046fbe75e2616d562bee14feaf28f52e0d4
Márton Áron <aron17marton@gmail.com>
2025-07-28 18:09:34 +0200

    feat: first commit

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/getlantern/systray"
+)
+
+func onReady() {
+
+}
+
+func onExit() {
+	systray.Quit()
+}
+
+func main() {
+	systray.Run(onReady, onExit)
+}
